5e77c06ad7086bf58ac8598a5015f8e4e60642b4
Daniel Lourenço Curado <dlcurado@gmail.com>
2017-11-28 09:28:09 -0300

    Removidos campos de hora de montagem e desmontagem do evento (ficam diretamente no local). Removido o menu Historicos e adicionado a opção para cadastrar Tipos de Eventos

---
+++ db/migrate/20171128211844_remove_hora_montagem_from_eventos.rb
@@ -0,0 +1,6 @@
+class RemoveHoraMontagemFromEventos < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :eventos, :hora_montagem, :datetime
+    remove_column :eventos, :hora_desmontagem, :datetime
+  end
+end
