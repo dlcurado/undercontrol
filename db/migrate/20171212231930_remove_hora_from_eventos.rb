ce79123af972d6a2e2058733d9eecac1723ba0d2
Daniel Lourenço Curado <dlcurado@gmail.com>
2017-12-15 15:29:35 -0200

    Removido migration de remoção de horas

--- db/migrate/20171212231930_remove_hora_from_eventos.rb
+++
@@ -1,6 +0,0 @@
-class RemoveHoraFromEventos < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :eventos, :hora_montagem, :datetime
-    remove_column :eventos, :hora_desmontagem, :datetime
-  end
-end
